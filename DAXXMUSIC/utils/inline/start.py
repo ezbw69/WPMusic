9327d5238f31688da7e88f7c94b1c84febeea979
ezbw69 <lipfaris00@gmail.com>
2023-10-28 16:29:42 +0700

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -10,7 +10,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], url=f"t.me/worstpartyinc"),
         ],
     ]
     return buttons
@@ -27,10 +27,10 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], url=f"t.me/worstpartyinc"),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_6"], url=f"t.me/hero4in"),
             InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
         ],
     ]
